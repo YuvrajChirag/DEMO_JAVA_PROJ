b2be38ca8b5c41cedb1243c39268568d11f35675
Yuvraj Chirag <yuvrajchirag9se@gmail.com>
2026-03-15 22:55:24 +0530

    Implement CALC mini scripting engine interpreter

---
+++ src/main/java/AssignInstruction.java
@@ -0,0 +1,14 @@
+public class AssignInstruction implements Instruction {
+    private final String variableName;
+    private final Expression expression;
+
+    public AssignInstruction(String variableName, Expression expression) {
+        this.variableName = variableName;
+        this.expression = expression;
+    }
+
+    @Override
+    public void execute(Environment env) {
+        env.set(variableName, expression.evaluate(env));
+    }
+}
---
+++ src/main/java/TokenType.java
@@ -0,0 +1,23 @@
+public enum TokenType {
+    NUMBER,
+    STRING,
+    IDENTIFIER,
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    ASSIGN,      // :=
+    PRINT,       // >>
+    IF,          // ?
+    REPEAT,      // @
+    ARROW,       // =>
+    GT,
+    LT,
+    EQEQ,
+    LPAREN,
+    RPAREN,
+    NEWLINE,
+    INDENT,
+    DEDENT,
+    EOF
+}
